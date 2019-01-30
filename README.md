cf7edcafc2ac2c518f42be96d9e752105c1028e3
caiweiqing1990 <1261540192@qq.com>
2019-01-30 15:01:21 +0800

    mytest

---
+++ README.md
@@ -0,0 +1,3 @@
+git add .
+git commit -m "satfi"
+git push -u origin master
--- mytest/mytest.c
+++ mytest/mytest.c
@@ -14,55 +14,53 @@
 
 static int getBaudrate(int baudrate)
 {
-	switch(baudrate) {
-	case 0: return B0;
-	case 50: return B50;
-	case 75: return B75;
-	case 110: return B110;
-	case 134: return B134;
-	case 150: return B150;
-	case 200: return B200;
-	case 300: return B300;
-	case 600: return B600;
-	case 1200: return B1200;
-	case 1800: return B1800;
-	case 2400: return B2400;
-	case 4800: return B4800;
-	case 9600: return B9600;
-	case 19200: return B19200;
-	case 38400: return B38400;
-	case 57600: return B57600;
-	case 115200: return B115200;
-	case 230400: return B230400;
-	case 460800: return B460800;
-	case 500000: return B500000;
-	case 576000: return B576000;
-	case 921600: return B921600;
-	case 1000000: return B1000000;
-	case 1152000: return B1152000;
-	case 1500000: return B1500000;
-	case 2000000: return B2000000;
-	case 2500000: return B2500000;
-	case 3000000: return B3000000;
-	case 3500000: return B3500000;
-	case 4000000: return B4000000;
-	default: return -1;
+	switch(baudrate) 
+	{
+		case 0: return B0;
+		case 50: return B50;
+		case 75: return B75;
+		case 110: return B110;
+		case 134: return B134;
+		case 150: return B150;
+		case 200: return B200;
+		case 300: return B300;
+		case 600: return B600;
+		case 1200: return B1200;
+		case 1800: return B1800;
+		case 2400: return B2400;
+		case 4800: return B4800;
+		case 9600: return B9600;
+		case 19200: return B19200;
+		case 38400: return B38400;
+		case 57600: return B57600;
+		case 115200: return B115200;
+		case 230400: return B230400;
+		case 460800: return B460800;
+		case 500000: return B500000;
+		case 576000: return B576000;
+		case 921600: return B921600;
+		case 1000000: return B1000000;
+		case 1152000: return B1152000;
+		case 1500000: return B1500000;
+		case 2000000: return B2000000;
+		case 2500000: return B2500000;
+		case 3000000: return B3000000;
+		case 3500000: return B3500000;
+		case 4000000: return B4000000;
+		default: return -1;
 	}
 }
 
 int init_serial(int *fd, char *device, int baud_rate)
 {
-	int ret;
 	printf("open serial port : %s ...\n", device);
 	int fd_serial = open(device, O_RDWR | O_NOCTTY | O_NDELAY);
 	if (fd_serial < 0)
 	{
 		perror("open fd_serial");
 		return -1;
-	} 
-
+	}
 	*fd = fd_serial;
-
 	/* 串口主要设置结构体termios <termios.h> */
 	struct termios options;
 
@@ -70,9 +68,7 @@ int init_serial(int *fd, char *device, int baud_rate)
 	*   参数fd为终端的文件描述符，返回的结果保存在termios结构体中
 	*/
 	tcgetattr(fd_serial, &options);
-
 	baud_rate = getBaudrate(baud_rate);
-	
 	/* 2.修改获得的参数 */
 	options.c_cflag |= CLOCAL | CREAD; /* 设置控制模块状态：本地连接，接收使能 */
 	options.c_cflag &= ~CSIZE;         /* 字符长度，设置数据位之前，一定要屏蔽这一位 */
@@ -82,20 +78,16 @@ int init_serial(int *fd, char *device, int baud_rate)
 	options.c_iflag |= IGNPAR;         /* 无奇偶校验 */
 	options.c_oflag = 0;               /* 输出模式 */
 	options.c_lflag = 0;               /* 不激活终端模式 */
-	ret = cfsetospeed(&options, baud_rate);    /* 设置波特率 */
-
+	cfsetospeed(&options, baud_rate);    /* 设置波特率 */
 	/* 3.设置新属性: TCSANOW，所有改变立即生效 */
 	tcflush(fd_serial, TCIFLUSH);      /* 溢出数据可以接收，但不读 */
 	tcsetattr(fd_serial, TCSANOW, &options);
-
-	printf("open serial port : %s ret = %d successfully!!!\n", device, ret);
+	printf("open serial port : %s successfully!!!\n", device);
 	return 0;
 }
 
 int satfd = 0;
-
 int flag = 0;
-
 static void *func_xx(void *p)
 {
   fd_set fds;
@@ -118,7 +110,7 @@ static void *func_xx(void *p)
 				data[n] = 0;
 				printf("%d %s\n", n, data);
 #else				
-				while(0)
+				while(1)
 				{
 				  if(idx==0)
 				  {
@@ -162,8 +154,8 @@ static void *func_xx(void *p)
 					}
 					else if(idx>4)
 					{
-						data[idx] = 0;
-						printf("%s", data);
+					  data[idx] = 0;
+					  printf("%s", data);
 					  if(strstr(data,"\n\nOK\n\n"))
 					  {
 						  if(data[idx-2]=='\n' && data[idx-1]=='\n')
@@ -217,7 +209,7 @@ int main(int argc, char *argv[])
 	if(argc != 4)
 	{
 		printf("mytest /dev/ttySAT0 115200 AT\n");
-		//return -1;
+		return -1;
 	}
 	
 	int baud_rate = atoi(argv[2]);
