d354522f8b057eeb7dc0a2f97476f476000d4958
caiweiqing1990 <1261540192@qq.com>
2019-10-11 16:13:22 +0800

    v8 files

--- files/path.sh
+++ files/path.sh
@@ -6,7 +6,9 @@ cp init.mt6735.rc $ANDROID_PATH/device/mediatek/mt6735/init.mt6735.rc
 cp device2.mk $ANDROID_PATH/device/mediatek/mt6735/device.mk
 cp satfi.te $ANDROID_PATH/system/sepolicy/satfi.te
 cp pppd.te $ANDROID_PATH/system/sepolicy/pppd.te
+cp file_contexts $ANDROID_PATH/system/sepolicy/file_contexts
 cp device.mk $ANDROID_PATH/device/lentek/len6737t_66_sh_n/device.mk
+cp ProjectConfig.mk $ANDROID_PATH/device/lentek/len6737t_66_sh_n/ProjectConfig.mk
 cp satbusy.wav $ANDROID_PATH/device/lentek/len6737t_66_sh_n/satbusy.wav
 cp busy.wav $ANDROID_PATH/device/lentek/len6737t_66_sh_n/busy.wav
 cp dudu.wav $ANDROID_PATH/device/lentek/len6737t_66_sh_n/dudu.wav
@@ -18,6 +20,13 @@ cp busybox $ANDROID_PATH/device/lentek/len6737t_66_sh_n/busybox
 cp libTTUpdate.so $ANDROID_PATH/device/lentek/len6737t_66_sh_n/libTTUpdate.so
 cp ipconfig.txt $ANDROID_PATH/device/lentek/len6737t_66_sh_n/ipconfig.txt
 cp EthernetConfigStore.java $ANDROID_PATH/frameworks/opt/net/ethernet/java/com/android/server/ethernet/EthernetConfigStore.java
+cp AudioSystem.java $ANDROID_PATH/frameworks/base/media/java/android/media/AudioSystem.java
+cp pseudo_mnl.c $ANDROID_PATH/vendor/mediatek/proprietary/hardware/connectivity/gps/mtk_mnld/mnl/src/pseudo_mnl.c
+cp kd_sensorlist.c $ANDROID_PATH/kernel-3.18/drivers/misc/mediatek/imgsensor/src/mt6735/kd_sensorlist.c
+
+cp custom_MemoryDevice.h $ANDROID_PATH/vendor/mediatek/proprietary/bootable/bootloader/preloader/custom/len6737t_66_sh_n/inc/custom_MemoryDevice.h
+
+cp MemoryDeviceList_MT6737T.xls $ANDROID_PATH/vendor/mediatek/proprietary/bootable/bootloader/preloader/tools/emigen/MT6735/MemoryDeviceList_MT6737T.xls
 
 cp AndroidManifest.xml $ANDROID_PATH/packages/apps/Settings/AndroidManifest.xml
 cp BootFinishBroadcastReceiver.java $ANDROID_PATH/packages/apps/Settings/src/com/mediatek/settings/BootFinishBroadcastReceiver.java
@@ -25,3 +34,4 @@ cp IMEIUtils.java $ANDROID_PATH/packages/apps/Settings/src/com/mediatek/sim/IMEI
 
 #cp codegen.dws $ANDROID_PATH/vendor/mediatek/proprietary/bootable/bootloader/lk/target/len6737t_66_sh_n/dct/dct/codegen.dws
 cp len6737t_66_sh_n_defconfig $ANDROID_PATH/kernel-3.18/arch/arm64/configs/len6737t_66_sh_n_defconfig
+
